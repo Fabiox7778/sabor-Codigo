83be9e3d31ba11574975d0035f8d31dfa5ab4785
gabi emi <gabiemi2021@gmail.com>
2026-03-06 11:48:59 -0300

    comeca a fazer integracao com o Open-Meteo

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -1,5 +1,28 @@
 import ClienteModel from '../models/clienteModel.js';
 
+export const buscarClima = async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (isNaN(id))
+            return res.status(400).json({ error: 'ID inválido. Informe um número válido.' });
+
+        const cliente = await ClienteModel.buscarPorId(parseInt(id));
+
+        if (!cliente) {
+            return res.status(404).json({ erro: 'Cliente não encontrado.' });
+        }
+
+        return res.status(200).json(cliente);
+    } catch (error) {
+        console.error(error);
+
+        return res.status(400).json({
+            erro: error.message,
+        });
+    }
+};
+
 export const criar = async (req, res) => {
     try {
         if (!req.body) {
@@ -19,91 +42,76 @@ export const criar = async (req, res) => {
             telefone,
             email,
             cpf,
-            cep
+            cep,
         });
 
         const clienteCriado = await cliente.criar();
         return res.status(201).json({
             message: 'Registro cadastrado com sucesso!',
-            clienteCriado
+            clienteCriado,
         });
-
     } catch (error) {
-        if (
-            error.message.includes("CPF") ||
-            error.message.includes("Telefone") ||
-            error.message.includes("CEP") ||
-            error.message.includes("ViaCEP") ||
-            error.message.includes("não encontrado")
-        ) {
-            return res.status(400).json({ erro: error.message });
-        }
-        return res.status(500).json({ error: 'Erro interno no servidor.' });
+        console.error(error);
+
+        return res.status(400).json({
+            erro: error.message,
+        });
     }
 };
 
 export const atualizar = async (req, res) => {
     try {
-
         const id = Number(req.params.id);
 
-        if (isNaN(id)) return res.status(400).json({ error: "ID inválido. Informe um número válido." });
+        if (isNaN(id))
+            return res.status(400).json({ error: 'ID inválido. Informe um número válido.' });
 
-        if (!req.body || Object.keys(req.body).length === 0) return res.status(400).json({
-            erro: "Envie pelo menos um campo para atualizar."
-        });
+        if (!req.body || Object.keys(req.body).length === 0)
+            return res.status(400).json({
+                erro: 'Envie pelo menos um campo para atualizar.',
+            });
 
         const cliente = new ClienteModel({
             id: parseInt(id),
-            ...req.body
+            ...req.body,
         });
 
         const clienteAtualizado = await cliente.atualizar();
 
         return res.status(200).json({
-            message: `O registro foi atualizado com sucesso!`, clienteAtualizado
+            message: `O registro foi atualizado com sucesso!`,
+            clienteAtualizado,
         });
-
     } catch (error) {
-        if (
-            error.message.includes("Cliente não encontrado") ||
-            error.message.includes("CPF") ||
-            error.message.includes("Telefone") ||
-            error.message.includes("CEP") ||
-            error.message.includes("ViaCEP") ||
-            error.message.includes("não encontrado")
-        ) {
-            return res.status(400).json({ erro: error.message });
-        }
-        res.status(500).json({ error: 'Erro interno no servidor.' });
+        console.error(error);
+
+        return res.status(400).json({
+            erro: error.message,
+        });
     }
 };
 
 export const deletar = async (req, res) => {
     try {
-
         const id = Number(req.params.id);
 
-        if (isNaN(id)) return res.status(400).json({ error: "ID inválido. Informe um número válido." });
+        if (isNaN(id))
+            return res.status(400).json({ error: 'ID inválido. Informe um número válido.' });
 
         const cliente = new ClienteModel({ id });
 
         const clienteApagado = await cliente.deletar();
 
         return res.status(200).json({
-            mensagem: "Cliente deletado com sucesso.",
-            clienteApagado
+            mensagem: 'Cliente deletado com sucesso.',
+            clienteApagado,
         });
-
     } catch (error) {
-        if (
-            error.message.includes("Cliente não encontrado") ||
-            error.message.includes("ABERTO")
-        ) {
-            return res.status(400).json({ erro: error.message });
-        }
+        console.error(error);
 
-        return res.status(500).json({ erro: "Erro interno do servidor." });
+        return res.status(400).json({
+            erro: error.message,
+        });
     }
 };
 
@@ -113,49 +121,52 @@ export const buscarTodos = async (req, res) => {
         const clientes = await ClienteModel.buscarTodos(filtros);
 
         if (clientes.length === 0) {
-            return res.status(200).json({ mensagem: "Nenhum cliente encontrado." });
+            return res.status(200).json({ mensagem: 'Nenhum cliente encontrado.' });
         }
 
         return res.status(200).json({
             total: clientes.length,
             message: 'Lista de clientes disponíveis',
             filtros,
-            clientes
+            clientes,
         });
-
     } catch (error) {
-        if (error.message.includes("Informe pelo menos")) {
-            return res.status(400).json({ erro: error.message });
-        }
+        console.error(error);
 
-        return res.status(500).json({ erro: "Erro interno do servidor." });
+        return res.status(400).json({
+            erro: error.message,
+        });
     }
 };
 
 export const buscarPorId = async (req, res) => {
     try {
-
         const id = Number(req.params.id);
 
-        if (isNaN(id)) return res.status(400).json({ error: "ID inválido. Informe um número válido." });
+        if (isNaN(id))
+            return res.status(400).json({ error: 'ID inválido. Informe um número válido.' });
 
         const cliente = await ClienteModel.buscarPorId(parseInt(id));
 
         if (!cliente) {
-            return res.status(404).json({ erro: "Cliente não encontrado." });
+            return res.status(404).json({ erro: 'Cliente não encontrado.' });
         }
 
         return res.status(200).json(cliente);
-
     } catch (error) {
-        return res.status(500).json({ erro: "Erro interno do servidor." });
+        console.error(error);
+
+        return res.status(400).json({
+            erro: error.message,
+        });
     }
-}
+};
 
 export default {
+    buscarClima,
     criar,
     atualizar,
     deletar,
     buscarTodos,
-    buscarPorId
+    buscarPorId,
 };
